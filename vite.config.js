d2eb735921b59febbe7ccd38971a961b7093f5c1
Sean Fife <seanfifeexplores@gmail.com>
2025-04-18 00:16:47 -0400

    Fix white screen: updated vite config and router for Vercel

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/soft-skills-site/', // 👈 this must match the repo name exactly
+  base: '/', // 👈 this must match the repo name exactly
   plugins: [react()],
 })
