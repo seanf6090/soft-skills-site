c934881008e5e3b7df765c1e1a433b73096bb8f5
Sean Fife <seanfifeexplores@gmail.com>
2025-04-17 23:44:22 -0400

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/soft-skills-site/', // 👈 this must match the repo name exactly
   plugins: [react()],
 })
